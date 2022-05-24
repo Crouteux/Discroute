8a7e737d979fcd1b9a0227ebea99afd69754057f
Joseph <joseph300905@gmail.com>
2022-05-24 19:18:26 +0000

    Fix feur listener

--- src/listeners/feur.spec.ts
+++ src/listeners/feur.spec.ts
@@ -1,20 +1,41 @@
 import test from 'ava';
 import sinon from 'sinon';
 
-import { Client, Message, TextChannel } from 'discord.js';
+import { Message, TextChannel, User } from 'discord.js';
 
 import feur from './feur';
 
-test('Replies', (t) => {
-    const c = sinon.createStubInstance(TextChannel);
-    const m = sinon.createStubInstance(Message);
+let c: sinon.SinonStubbedInstance<TextChannel>;
+let m: sinon.SinonStubbedInstance<Message>;
+let u: sinon.SinonStubbedInstance<User>;
+
+test.beforeEach((t) => {
+    c = sinon.createStubInstance(TextChannel);
+    m = sinon.createStubInstance(Message);
+    u = sinon.createStubInstance(User);
     sinon.stub(m, 'channel').get(() => c);
-    m.content = "i was fourteen when i met her"
+    m.author = u;
+});
+
+test.afterEach.always((t) => {
+    sinon.resetHistory();
+});
+
+test.serial('Replies', (t) => {
+    m.content = 'i was fourteen when i met her';
 
     feur.handler(m);
 
-    t.is(
-        c.send.getCall(0).args[0], 'feurteen'
-    );
+    t.is(c.send.getCall(0).args[0], 'feurteen');
 });
 
+test.serial('Answer count', (t) => {
+    m.content = 'for what';
+
+    const count = 5;
+    for (let i = 0; i < 5; i++) {
+        feur.handler(m);
+    }
+
+    t.is(c.send.callCount, count);
+});
--- src/listeners/feur.ts
+++ src/listeners/feur.ts
@@ -4,13 +4,15 @@ const reg = /f[aeiou]*r/g;
 const feur: Handler<'messageCreate'> = {
     on: 'messageCreate',
     handler: (message) => {
+        if (message.author.bot) return;
+
         const feur = message.content
             .toLowerCase()
             .split(' ')
-            .find((word) => reg.test(word) && !word.includes("feur"))
+            .find((word) => reg.test(word) && !word.includes('feur'))
             ?.replace(reg, 'feur');
 
-        if (feur) message.channel.send(feur)
+        if (feur) message.channel.send(feur);
     },
 };
 
