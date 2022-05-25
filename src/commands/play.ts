cc5b4470d5faae0cca808dde1c03abe61e62ab11
Joseph <joseph300905@gmail.com>
2022-05-25 21:49:04 +0200

    Implement YouTube search

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -2,6 +2,8 @@ import { Command } from '../command';
 
 import { playYoutube } from '../lib/player';
 
+import yts from 'yt-search';
+
 export const play: Command = {
     name: 'play',
     description: 'Plays a song',
@@ -12,8 +14,19 @@ export const play: Command = {
                 'Vous devez être connecté à un salon vocal pour utiliser cette commande'
             );
 
-        const url = args[0];
-        if (!url) return message.channel.send("L'URL n'est pas valide");
+        if (args.length === 0)
+            return message.channel.send('Veuillez entrer une URL');
+
+        let url: string;
+        if (
+            args[0].startsWith('https://www.youtube.com/watch?v=') ||
+            args[0].startsWith('https://youtu.be/')
+        )
+            url = args[0];
+        else {
+            const res = await yts(args.join(' '));
+            url = res.videos[0].url;
+        }
 
         playYoutube(url, voiceChannel);
     },
