64e2687132b386686f64e719a578c2c11ea9d84c
Cringlepoob <105988707+Cringlepoob@users.noreply.github.com>
2022-05-26 17:14:58 +0200

    Create Blanc.ts

---
+++ src/listeners/Blanc.ts
@@ -0,0 +1,14 @@
+import { Handler } from '../listener';
+
+const blnc = "** **";
+const blanc: Handler<'messageCreate'> = {
+    on: 'messageCreate',
+    handler: (message) => {
+        if (message.author.id == "717414214368755783"){
+            message.channel.send(blnc)
+        }
+         ;
+    },
+};
+
+export default blanc;
